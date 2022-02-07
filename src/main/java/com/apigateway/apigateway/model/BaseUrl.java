e6ac47ba528971812a786612fc86478f9d522bf3
Parnal Patil <parnal.patil@gmail.com>
2022-02-06 19:49:00 -0500

    Testing for docker

--- src/main/java/com/apigateway/apigateway/model/BaseUrl.java
+++ src/main/java/com/apigateway/apigateway/model/BaseUrl.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BaseUrl {
-    public String Plotting = "http://localhost:5000";
-    public String Logger = "http://localhost:8085";
+//    public String Plotting = "http://localhost:5000";
+//    public String Logger = "http://localhost:8080";
+//    public String Registry = "http://localhost:3333";
+
+
+    public String Plotting = "http://7276-2601-408-c401-3903-41ba-f2b3-e651-44ee.ngrok.io";
+    public String Logger = "http://6bbf-2601-408-c401-3903-41ba-f2b3-e651-44ee.ngrok.io";
     public String Registry = "http://localhost:3333";
+
+
 //  Registry on Tanu's Registry
 //    public String Registry = "http://e4a1-66-244-80-73.ngrok.io";
 
