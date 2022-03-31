493a3dbc5abf171b6b5ba76a8a821b417a0edc02
Parnal Patil <parnal.patil@gmail.com>
2022-03-30 21:16:07 -0400

    api gateway with new microservice

--- src/main/java/com/apigateway/apigateway/model/BaseUrl.java
+++ src/main/java/com/apigateway/apigateway/model/BaseUrl.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BaseUrl {
-//    public String Plotting = "http://localhost:5000";
+    public String Plotting = "http://localhost:5000";
 //    public String Logger = "http://localhost:8080";
-//    public String Registry = "http://localhost:3333";
-
-
-    public String Plotting = "http://4a99-2001-18e8-2-28b6-e860-f7c1-1087-b608.ngrok.io";
-    public String Logger = "http://0bed-2001-18e8-2-28b6-e860-f7c1-1087-b608.ngrok.io";
     public String Registry = "http://localhost:3333";
+
+
+//    public String Plotting = "http://4a99-2001-18e8-2-28b6-e860-f7c1-1087-b608.ngrok.io";
+    public String Logger = "http://ddc3-2001-18e8-2-28b6-fdaa-7b54-33a1-8be.ngrok.io";
+//    public String Registry = "http://ddc3-2001-18e8-2-28b6-fdaa-7b54-33a1-8be.ngrok.io";
 
 
 
