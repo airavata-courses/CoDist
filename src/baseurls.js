0ba04f65e69c45e178144cd80066eaf6f54d2ef4
Aditya Arun Rudrawar <arudrawa@iu.edu>
2022-04-06 22:50:50 -0400

    BaseURL Change

--- src/baseurls.js
+++ src/baseurls.js
@@ -1,2 +1,2 @@
 // export var baseUrl = "https://e0c7-2601-801-103-1100-fc67-f65d-3f7-558e.ngrok.io";
-export var baseUrl = "http://gateway-service:8069";
+export var baseUrl = "http://localhost:8069";
